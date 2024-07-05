7662429d244d3c44e5677ff9670dba129897f92e
moduri <dm029@naver.com>
2024-07-06 00:59:52 +0900

    shorts 프로젝트 기본 설정 및 아이콘 추가

---
+++ app/src/main/java/com/yt/shorts/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yt.shorts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yt.shorts.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
