67b00718f163e6dcd89aabe5386fb663f714494e
moduri <dm029@naver.com>
2024-07-13 22:52:24 +0900

    UI 수정

--- app/src/main/java/com/yt/shorts/adapter/ShortsAdapter.java
+++ app/src/main/java/com/yt/shorts/adapter/ShortsAdapter.java
@@ -1,6 +1,7 @@
 package com.yt.shorts.adapter;
 
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,6 @@ import android.widget.MediaController;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.yt.shorts.R;
 import com.yt.shorts.databinding.RowShortsBinding;
 import com.yt.shorts.model.ShortsItem;
 
@@ -17,8 +17,9 @@ import java.util.List;
 
 public class ShortsAdapter extends RecyclerView.Adapter<ShortsAdapter.ShortsViewHolder> {
 
-    List<ShortsItem> shortsItems;
-    MediaController mediaController;
+    private List<ShortsItem> shortsItems;
+    private MediaController mediaController;
+    private int currentlyPlayingPosition = 0;
 
     public ShortsAdapter(List<ShortsItem> shortsItems) {
         this.shortsItems = shortsItems;
@@ -34,7 +35,7 @@ public class ShortsAdapter extends RecyclerView.Adapter<ShortsAdapter.ShortsView
     @Override
     public void onBindViewHolder(@NonNull ShortsViewHolder holder, int position) {
         ShortsItem item = shortsItems.get(position);
-        holder.bind(item);
+        holder.bind(item, position);
     }
 
     @Override
@@ -49,20 +50,58 @@ public class ShortsAdapter extends RecyclerView.Adapter<ShortsAdapter.ShortsView
         public ShortsViewHolder(@NonNull RowShortsBinding rowShortsBinding) {
             super(rowShortsBinding.getRoot());
             this.rowShortsBinding = rowShortsBinding;
-        }
 
-        void bind(ShortsItem item) {
-            // video 설정
-            Uri uri = Uri.parse(item.getUrl());
-            mediaController = new MediaController(itemView.getContext()); // ??
+            rowShortsBinding.getRoot().getContext();
+            mediaController = new MediaController(rowShortsBinding.getRoot().getContext());
             mediaController.setAnchorView(rowShortsBinding.vvShorts);
 
-            rowShortsBinding.vvShorts.setVideoURI(uri);
-            rowShortsBinding.vvShorts.start();
+            rowShortsBinding.vvShorts.setOnClickListener(v -> {
+                if (rowShortsBinding.vvShorts.isPlaying()) {
+                    rowShortsBinding.vvShorts.pause();
+                } else {
+                    rowShortsBinding.vvShorts.start();
+                }
+            });
+        }
 
+        void bind(ShortsItem item, int position) {
+
+            Uri uri = Uri.parse(item.getUrl());
+            rowShortsBinding.vvShorts.setVideoURI(uri);
+
+            if (currentlyPlayingPosition == position + 1) {
+                rowShortsBinding.vvShorts.start(); // 현재 아이템이면 재생 시작
+                mediaController.show(); // 비디오 컨트롤러 표시
+            } else {
+                rowShortsBinding.vvShorts.pause(); // 아니면 정지
+            }
+
+            // 비디오 끝나면 다시 재생할 수 있도록 처리
+            rowShortsBinding.vvShorts.setOnCompletionListener(mp -> {
+                rowShortsBinding.vvShorts.seekTo(0); // 비디오를 처음으로 돌려놓고
+                rowShortsBinding.vvShorts.pause(); // 일시 정지
+            });
+
+            itemView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+                @Override
+                public void onViewAttachedToWindow(View view) {
+                    rowShortsBinding.vvShorts.start();
+                }
+
+                @Override
+                public void onViewDetachedFromWindow(View view) {
+                        rowShortsBinding.vvShorts.pause();
+
+                }
+            });
+
+            // 클릭 이벤트 등을 설정하여 아이템 클릭 시 재생을 시작하도록 구현할 수도 있습니다.
+
+            // 기타 필요한 데이터 바인딩
             rowShortsBinding.tvMediaTitle.setText(item.getTitle());
             rowShortsBinding.tvUserName.setText("이름");
         }
 
     }
+
 }
